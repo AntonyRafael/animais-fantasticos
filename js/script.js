dc81ca13444a5e3cca54ea04b059c3de88cd26da
Antony Rafael <depaulacostaantony@gmail.com>
2021-01-17 12:56:05 -0300

    Accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -14,7 +14,9 @@ const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
 initAnimacaoScroll();
-initAccordion();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 initTabNav();
 initModal();
 initTooltip();
