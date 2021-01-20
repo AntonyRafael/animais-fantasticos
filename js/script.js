1737582da68e510854180a3edfbe6a639d45eef1
Antony Rafael <depaulacostaantony@gmail.com>
2021-01-19 21:44:05 -0300

    corrigido caminho json online

--- js/script.js
+++ js/script.js
@@ -37,6 +37,6 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('../../animais-api.json', '.numeros-grid');
+fetchAnimais('./animais-api.json', '.numeros-grid');
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
